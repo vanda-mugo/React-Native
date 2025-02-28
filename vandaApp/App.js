9f1a14e964720db3df0e3396ff9a0e809d56a1e7
vanda-mugo <johnmugo006@gmail.com>
2025-02-28 21:55:22 +0300

    Slight changes to the view component in the react native app

--- vandaApp/App.js
+++ vandaApp/App.js
@@ -4,10 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>I feel so good about this .</Text>
-      <Text>My name is John Mugo and this is my first mobile application</Text>
+      <View style={styles.header} />
+      <View style={styles.header} />
+      <View style={styles.header} />
+      <View style={styles.header} />
       <StatusBar style="auto" />
+
     </View>
   );
 }
@@ -16,9 +18,22 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#FFFF00',
-    alignItems: 'right',
+    alignItems: 'center',
     justifyContent: 'center',
-    margin: 10,
-    borderRadius: 30,
+    padding: 20,
+  },
+  header: {
+    backgroundColor: 'green',
+    height: 150,
+    width: 400,
+    margin:10,
+    borderRadius: 10,
   },
 });
+
+
+/**
+ * <Text>I feel so good about this.</Text>
+      <Text>Richard Mugo</Text>
+      <Text>My name is John Mugo and this is my first mobile application</Text>
+ */
