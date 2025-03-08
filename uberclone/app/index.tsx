c8d439a8608e91d000a4fbdbb73894ae024a0f29
vanda-mugo <johnmugo006@gmail.com>
2025-03-08 20:41:09 +0300

    Added uber app

---
+++ uberclone/app/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+import "../assets/global.css";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>Home Screen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
