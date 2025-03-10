033b0fbcf9f2a7054e84c3cd85e1bd70a5c3237d
vanda-mugo <johnmugo006@gmail.com>
2025-03-10 17:23:49 +0300

    Added the tab to the application

---
+++ stickerSmash/StickerSmash/app/(tabs)/_layout.tsx
@@ -0,0 +1,31 @@
+import { Tabs } from 'expo-router';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { StatusBar } from 'react-native';
+
+export default function TabLayout() {
+  return (
+    <>
+    <StatusBar backgroundColor="#25292e" barStyle="light-content" />
+    <Tabs
+          screenOptions={{
+            tabBarActiveTintColor: '#ffd33d',
+            headerStyle: {
+              backgroundColor: '#25292e',
+            },
+            headerShadowVisible: false,
+            headerTintColor: '#fff',
+            tabBarStyle: {
+              backgroundColor: '#25292e',
+            },
+        }}
+      >
+      <Tabs.Screen name="index" options={{ title: 'Home', tabBarIcon: ({color, focused}) => (
+        <Ionicons name={focused ? 'home-sharp' : 'home-outline'} size={24} color={color} />) 
+        }} />
+      <Tabs.Screen name="about" options={{ title: 'About', tabBarIcon: ({color, focused}) => (
+        <Ionicons name={focused ? 'information-circle' : 'information-circle-outline'} size={24} color={color} />) 
+        }} />
+    </Tabs>
+    </>
+  );
+}
