2af3d516eb645e408114182cebf0670ba8365644
vanda-mugo <johnmugo006@gmail.com>
2025-03-11 10:39:58 +0300

    Added sticker adding modal and functionality

--- stickerSmash/StickerSmash/app/(tabs)/index.tsx
+++ stickerSmash/StickerSmash/app/(tabs)/index.tsx
@@ -6,11 +6,20 @@ import { Text, View, StyleSheet } from 'react-native';
  import Button from '@/components/Button';
  import * as ImagePicker from 'expo-image-picker';
  import { useState } from 'react';
+ import CircleButton from '@/components/CircleButton';
+ import IconButton from '@/components/IconButton';
+ import EmojiPicker from '@/components/EmojiPicker';
+ import { type ImageSource } from 'expo-image';
+ import EmojiList from '@/components/EmojiList';
+ import EmojiSticker from '@/components/EmojiSticker';
 
  const placeholderImage = require("../../assets/images/background-image.png");
 
 export default function Index() {
   const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+  const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+  const [pickedEmoji, setPickedEmoji] = useState<ImageSource | undefined>(undefined);
   NavigationBar.setBackgroundColorAsync("#25292e");
 
   const pickImageAsync = async () => {
@@ -22,20 +31,53 @@ export default function Index() {
     if (!result.canceled) {
       console.log(result);
       setSelectedImage(result.assets[0].uri) ;
+      setShowAppOptions(true);
     }else{
       alert("you did not select an image");
     }
   };
 
+  const onReset = () => { 
+    setShowAppOptions(false);
+  };
+
+  const onAddSticker = () => {
+    setIsModalVisible(true);
+  };
+
+  const onSaveImageAsync = async() => {
+    // to implement later
+  };
+
+  const onModalClose = () => {
+    setIsModalVisible(false);
+  };
+
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={selectedImage || placeholderImage} />
+        {pickedEmoji && <EmojiSticker imageSize={30} stickerSource={pickedEmoji} />}
       </View>
-      <View style={styles.footerContainer}>
+      {showAppOptions ? (
+      <View>
+        <View style={styles.optionsContainer}>
+          <View style={styles.optionsRow}>
+            <IconButton icon="refresh" label="Reset" onPress={onReset} />
+            <CircleButton onPress={onAddSticker} />
+            <IconButton icon="save-alt" label="Save" onPress={onSaveImageAsync} />
+          </View>
+        </View>
+      </View>) : 
+      (<View style={styles.footerContainer}>
         <Button label="Choose a photo" theme="primary" onPress={pickImageAsync}/> 
-        <Button label='Use this photo' />
-      </View>
+        <Button label='Use this photo' onPress={() => (setShowAppOptions(true))}/>
+      </View>)
+      }
+      <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+        <EmojiList onSelect={(emoji) => setPickedEmoji(emoji)} onCloseModal={onModalClose} />
+      </EmojiPicker>
     </View>
   );
 }
@@ -53,6 +95,14 @@ const styles = StyleSheet.create({
   footerContainer:{
     flex: 1 / 3,
     alignItems: 'center',
+  },
+  optionsContainer: {
+    //position: 'absolute',
+    bottom: 80,
+  },
+  optionsRow: {
+    alignItems: 'center',
+    flexDirection: 'row',
   },
 });
 
